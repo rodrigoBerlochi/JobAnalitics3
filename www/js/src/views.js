f8eae40b783f552559dd2681705849ed77089b1a
Rodrigo Berlochi <rodrigo.berlochi@razorfish.com>
2015-01-05 17:53:52 -0300

    adding UI semantic components, more backbone code on Views

--- www/js/src/views.js
+++ www/js/src/views.js
@@ -1,7 +1,30 @@
-var ProfileView = Backbone.View.extend({
+var ProfileFormView = Backbone.View.extend({
 
 });
 
+var ProfileBriefView = Backbone.View.extend({
+    
+    el: '#wrapper'
+    
+    template: $(),
+    
+    initialize: function() {
+    
+        //skipping it out here since this view uses stickit
+        //this.listenTo(this.model, 'change', this.render);
+    },
+    
+    events: {},
+    
+    bindings: {},
+    
+    render: function() {
+        this.$el.html(this.template);
+        this.stickit();
+        return this;
+    }
+    
+});
 
 var ResultSetView = Backbone.View.extend({
 
